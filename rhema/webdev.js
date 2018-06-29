b216c250294edf755a1765d77df6d6350eb9cb84
annikale <40610903+annikale@users.noreply.github.com>
2018-06-29 15:28:06 -0400

    Add files via upload

---
+++ rhema/webdev.js
@@ -0,0 +1,7 @@
+
+//say hi and intro
+//Listen for the click on the "Click Me" button
+document.getElementById("click-button").addEventListener("click", function(){
+  //use alert to say hi and intro
+  alert("Hi, this is my lookbook!");
+});
